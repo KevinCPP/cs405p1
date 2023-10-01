95d72498a348698c54018dfe1d81ff4395787b67
KevinCPP <kevinncosby2@gmail.com>
2023-10-01 18:13:16 -0400

    fixed a bug with credentials

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from addresses_generator import generate_text_file
 from addresses import create_addresses_table
 
-class sql_credentials:
+class SQL_Credentials:
     def __init__(self):
         self.username = "knco259"
         self.password = "<password>"
@@ -11,6 +11,9 @@ if __name__ == "__main__":
     # generate addresses.txt
     generate_text_file()
     
+    # create obj for sql credentials
+    sql_creds = SQL_Credentials()
+
     # create addresses table
-    create_addresses_table(sql_credentials)
+    create_addresses_table(sql_creds)
       
